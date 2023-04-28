48f9788185ffe7ae871e476a30910c9422d30688
Rickwang574 <108665852+Code2Research@users.noreply.github.com>
2023-04-28 10:00:10 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,24 +6,35 @@ from deform2self import deform2self, deform2self_sequence
 from utils import add_gaussian_noise, img_float2int
 import yaml
 
-
 if __name__ == '__main__':
 
+    # Count the number of gpu_available
+    gpu_count = torch.cuda.device_count()
+    print("The number of gpu_available is: ", gpu_count)
+
+    # Check the current gpu
+    gpu_current = torch.cuda.current_device()
+    print("The current gpu is: ", gpu_count)
+
+    # distributed computing ?
+    gpu_distributed = torch.distributed.is_initialized()
+    print("The current computing type is: ", gpu_distributed)
+
     with open('config.yaml', 'r') as stream:
-        config = yaml.safe_load(stream)    
-    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]=config['gpu']
+        config = yaml.safe_load(stream)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = config['gpu']
 
     imgs = []
     for fname in sorted(os.listdir('./data')):
         imgs.append(np.array(Image.open(os.path.join('./data', fname))))
     imgs = np.stack(imgs, axis=0).astype(np.float32)
     imgs /= imgs.max()
-    imgs = torch.from_numpy(imgs).unsqueeze(1) # Tx1xHxW
+    imgs = torch.from_numpy(imgs).unsqueeze(1)  # Tx1xHxW
 
     noisy = add_gaussian_noise(imgs, 15)
-    
-    denoised = deform2self_sequence(noisy, config) # Tx1xHxW
+
+    denoised = deform2self_sequence(noisy, config)  # Tx1xHxW
 
     denoised = denoised.squeeze(1).cpu().numpy()
     denoised = img_float2int(denoised)
@@ -34,11 +45,3 @@ if __name__ == '__main__':
     for i, (img_n, img_d) in enumerate(zip(noisy, denoised)):
         Image.fromarray(img_n).save('outputs/noisy%d.png' % i)
         Image.fromarray(img_d).save('outputs/denoised%d.png' % i)
-    
-
-
-
-
-
-
-     
